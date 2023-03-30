44948e904dff8828837bb2df8629021ba6618d71
carson-eeee <129293282+carson-eeee@users.noreply.github.com>
2023-03-30 17:31:49 +0800

    e
    e

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("haha");
+function changeText(){
+    var mytext =document.getElementById("mytext")
+    // mytext.innerHTML="You clicked";
+    window.open("https://github.com/carson-eeee/carson-eeee.github.io")
+  }
+  function imgclick(){
+    console.log("hello world")
+  }
+  
