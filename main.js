ec05e7b333c224c50caf6795d3c1159365a450f3
ArkCarcar <leungwchw151063@gmail.com>
2023-03-31 13:10:12 +0800

    new prompt

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ function changeText(){
     // mytext.innerHTML="You clicked";
     window.open("https://github.com/carson-eeee/carson-eeee.github.io")
   }
-  function imgclick(){
-    console.log("hello world")
+function imgclick(){
+  var imgprompt = document.getElementById("prompt")
+  imgprompt.innerHTML="rrrr";
+  console.log("hello world")
   }
-  
+document.getElementById("myButton").onclick = function(){
+  var myName = document.getElementById("myText").value;
+  var print = document.getElementById("print");
+  print.innerHTML=("hello",myName);
+}
